4bca20ae443a734fd5f382cb0e411710fd620554
usama <usamahafeez948@gmail.com>
2024-02-23 13:33:16 +0500

    final

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port);
   console.log('server is running at port 3000')
   
 }
